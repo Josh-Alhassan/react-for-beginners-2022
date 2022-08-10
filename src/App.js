bb255ed7da43c60697ae7dd2bfd195a7ebd21a0e
Saint-Josh <saintjosh26@gmail.com>
2022-08-11 00:42:31 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import Hello from './Components/Hello'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <Hello />
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ src/Components/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hello = () => {
+  return (
+    <>
+      <h1> What's Up World </h1>
+    </>
+  )
+}
+
+export default Hello;
