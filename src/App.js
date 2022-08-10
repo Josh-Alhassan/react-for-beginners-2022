3c50a06fc40946aae34c24bb29c88c38728b58d2
Saint-Josh <saintjosh26@gmail.com>
2022-08-11 00:58:14 +0100

    react Class component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./style.css";
 import Hello from './Components/Hello'
+import Message from './Components/Message';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <Hello />
+      <Message />
       <p>Start editing to see some magic happen :)</p>
+
     </div>
   );
 }
---
+++ src/Components/Message.js
@@ -0,0 +1,13 @@
+// Importing React, Component from 'react'
+import React from 'react';
+import { Component } from 'react';
+
+class Message extends Component {
+  render() {
+    return (
+      <h2> Thi is a class component </h2>
+    )
+  }
+}
+
+export default Message;
