fa7f8f830f6697cf5aa917b452f98ae91b10c92f
Saint-Josh <saintjosh26@gmail.com>
2022-08-11 01:12:01 +0100

    jsx intro

--- src/Components/Hello.js
+++ src/Components/Hello.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+const name = 'Joshua Alhassan';
+
+const displayMessage = () => {
+  return " I need help understanding react";
+}
 const Hello = () => {
   return (
     <>
       <h1> What's Up World </h1>
+      <p> My name is {name} and {displayMessage()}</p>
+      <p> I can do 2 + 2 = {2+2} </p>
     </>
   )
 }
